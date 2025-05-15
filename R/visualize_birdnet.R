0db8f3a43efbf1d51cf8cba9f5dfb147ae57e96f
Sachserf <f.sachser@gmx.at>
2025-05-15 11:09:51 +0200

    edit plotheight and species_list.txt

--- R/visualize_birdnet.R
+++ R/visualize_birdnet.R
@@ -7,16 +7,19 @@
 #' @export
 visualize_birdnet <- function(BirdNET_selection_table, dirname.target.figures) {
 
-df <- rio::import(BirdNET_selection_table)   #### substitude with base function?!
+df <- rio::import(BirdNET_selection_table)
 
 df <- df %>%
   dplyr::mutate(bins = cut(Confidence, breaks = seq(0, 1, length.out = 11), labels = FALSE, include.lowest = TRUE)/10)
 
+plotheight <- length(unique(df$`Common Name`))/10
+
 df <- df %>%
   dplyr::group_by(`Common Name`) %>%
-  dplyr::mutate(mean = mean(Confidence)) %>%
-  dplyr::ungroup() %>%
-  dplyr::distinct(`Common Name`, mean) %>%
+  dplyr::summarise(mean = mean(Confidence, na.rm = TRUE), .groups = "drop") %>%
+#  dplyr::mutate(mean = mean(Confidence)) %>%
+#  dplyr::ungroup() %>%
+#  dplyr::distinct(`Common Name`, mean) %>%
   dplyr::arrange(mean) %>%
   tibble::rowid_to_column() %>%
   dplyr::select(-mean) %>%
@@ -33,7 +36,7 @@ fig1 <- df %>%
   ggplot2::theme_minimal() +
   ggplot2::theme(panel.grid.minor = ggplot2::element_blank())
 
-ggplot2::ggsave(fig1, filename = file.path(dirname.target.figures, "speciesBins.png"), height = 13)
+ggplot2::ggsave(fig1, filename = file.path(dirname.target.figures, "speciesBins.png"), height = plotheight)
 
 fig2 <- df %>%
   ggplot2::ggplot() +
@@ -43,5 +46,5 @@ fig2 <- df %>%
   ggplot2::theme_minimal() +
   ggplot2::theme(panel.grid.minor = ggplot2::element_blank())
 
-ggplot2::ggsave(fig2, filename = file.path(dirname.target.figures, "speciesConfidence.png"), height = 13)
+ggplot2::ggsave(fig2, filename = file.path(dirname.target.figures, "speciesConfidence.png"), height = plotheight)
 }
